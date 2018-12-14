f8cb2417ff667c1273f775058b197426bcfcc57a
Ivo Katunaric <ivo@coreline.agency>
2018-12-14 14:06:07 +0100

    Use imagemagick for resizing

--- resize-image.js
+++ resize-image.js
@@ -1,6 +1,14 @@
 import Jimp from 'jimp';
-import {createThumbFile, thumbExists} from "./aws-manager";
-import {checkUrl} from "./check-url";
+import tmp from 'tmp-promise';
+import imagemagick from 'imagemagick';
+import { promisify } from 'util';
+import cleanDeep from 'clean-deep';
+import { createThumbFile, thumbExists } from "./aws-manager";
+import { checkUrl } from "./check-url";
+
+const downloadToFile = promisify(require('download-to-file'));
+const resize = promisify(imagemagick.resize);
+const readFile = promisify(require('fs').readFile);
 
 export async function resizeImage(req, res) {
   const { url, w, h } = req.query;
@@ -19,9 +27,21 @@ export async function resizeImage(req, res) {
     return;
   }
 
-  const image = await Jimp.read(url);
-  const resized = image.resize(parseInt(w, 10) || Jimp.AUTO, parseInt(h, 10) || Jimp.AUTO);
-  const buffer = await resized.getBufferAsync(Jimp.MIME_JPEG);
-  const resultingUrl = await createThumbFile(buffer, url, w, h);
-  res.redirect(302, resultingUrl);
+  const inputFile = await tmp.file();
+  const outputFile = await tmp.file();
+  try {
+    await downloadToFile(url, inputFile.path);
+    await resize(cleanDeep({
+      srcPath: inputFile.path,
+      dstPath: outputFile.path,
+      width: w,
+      height: h,
+    }));
+    const buffer = await readFile(outputFile.path);
+    const resultingUrl = await createThumbFile(buffer, url, w, h);
+    res.redirect(302, resultingUrl);
+  } finally {
+    inputFile.cleanup();
+    outputFile.cleanup();
+  }
 }
