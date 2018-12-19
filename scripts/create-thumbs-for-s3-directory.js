bc807dc6339914b91b75de4f5803e4b30dcca266
Ivo Katunaric <ivo@coreline.agency>
2018-12-19 10:25:34 +0100

    Introduce timeout and retry

--- scripts/create-thumbs-for-s3-directory.js
+++ scripts/create-thumbs-for-s3-directory.js
@@ -1,7 +1,10 @@
 import 'dotenv/config';
 import normalizeUrl from 'normalize-url';
+import bluebird from 'bluebird';
+import { timeout } from 'promise-timeout';
 import {listUrlsByPrefix} from "../aws-manager";
 import {resizeFromUrl} from "../resize-from-url";
+
 
 const usage = `
   USAGE: nnode scripts/create-thumbs-for-s3-directory <directory> <start-index> <width or auto> <height or auto>
@@ -28,8 +31,25 @@ if (!width && !height) {
   console.log(`Total images ${allUrls.length}`);
   for (let i = startIndex; i < allUrls.length; i += 1) {
     const url = normalizeUrl(allUrls[i]);
-    console.log('resizing:');
-    console.log(url);
-    console.log('resize OK for index', i, await resizeFromUrl(url, undefined, 150));
+    let occurredError;
+    let attemptCount = 1;
+    do {
+      if (attemptCount > 10) {
+        console.error('Skipping index', i, url);
+        break;
+      }
+      occurredError = false;
+      try {
+        console.log('resizing attempt', attemptCount);
+        console.log(url);
+        const resizeUrl = await timeout(resizeFromUrl(url, width, height), 15000);
+        console.log('resize OK for index', i, resizeUrl);
+      } catch (e) {
+        console.error('error', e);
+        await bluebird.delay(5000);
+        occurredError = true;
+        attemptCount += 1;
+      }
+    } while (occurredError);
   }
 })();
